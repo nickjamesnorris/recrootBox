efa47f7a8af2b34ccfdf6d5d5abd3b67b9d4b65f
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-04-16 16:30:58 -0500

    Personal details changed

--- src/components/EmailCaptureSection.js
+++ src/components/EmailCaptureSection.js
@@ -1,91 +1,86 @@
-import React, {useState, useEffect }from 'react';
-import { showEmailCapture, accent } from './ProfileInformation'
-import RbButton from './RbButton';
-import './app.css';
-import styled from 'styled-components';
-
-
+import React, { useState, useEffect } from "react"
+import { showEmailCapture, accent } from "./ProfileInformation"
+import RbButton from "./RbButton"
+import "./app.css"
+import styled from "styled-components"
 
 const nouns = [
-    'space', 
-    'France', 
-    'out', 
-    'THERE!!!', 
-    'foo', 
-    'buzz', 
-    'whatever', 
-    ' ........ ',  
-    '@!#$', 
-    'HERE!!!',
-    'existence',
-    'fear',
-    'giraffe',
-    'voyage',
-    'station',
-    'expansion',
-    '💯💯💯💯💯',
-    `${accent}`
-];
-
+  "learning",
+  "crazy",
+  "on hold",
+  "chaos",
+  "foo/bar",
+  "good",
+  "energy",
+  " ........ ",
+  "perfect",
+  "HERE!!!",
+  "existence",
+  "fear",
+  "voyage",
+  "expansion",
+  "💯💯💯💯💯",
+]
 
 const EmailCaptureSection = () => {
-    const [noun, setNoun] = useState("touch")
+  const [noun, setNoun] = useState("touch")
 
-    const wordSwitch = () => {
-        setInterval(() => {
-            setNoun(nouns[Math.floor(Math.random() * nouns.length)]);
-        }, 1500);
-    }
+  const wordSwitch = () => {
+    setInterval(() => {
+      setNoun(nouns[Math.floor(Math.random() * nouns.length)])
+    }, 1500)
+  }
 
+  useEffect(() => {
+    wordSwitch()
+  }, [])
 
-    useEffect(() => {
-        wordSwitch()
-    }, [])
-
-    return (
-    showEmailCapture ?
-        <Row>
-        <h3>Stay in <span style={{textDecoration: `underline`}}>{noun}</span>!</h3>
-        <FormRow>
-            <form name="contact" method="post" data-netlify="true" action="/success">
-            <input type="hidden" name="form-name" value="contact" />
-            <Input  placeholder="email..."  name="email" />
-            <RbButton text={`Go!`} type="submit"/>
-            </form>
-        </FormRow>
-     </Row>
-      : null
-  )
+  return showEmailCapture ? (
+    <Row>
+      <h3>
+        Life is <span style={{ textDecoration: `underline` }}>{noun}</span>!
+      </h3>
+      <FormRow>
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          action="/success"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <Input placeholder="email..." name="email" />
+          <RbButton text={`Go!`} type="submit" />
+        </form>
+      </FormRow>
+    </Row>
+  ) : null
 }
 
-export default EmailCaptureSection;
-
+export default EmailCaptureSection
 
 const Row = styled.div`
-    display:flex;
-    flex-direction: column;
-    align-items:center;
-    justify-content:center;
-    margin-top: 0;
-    padding-top: 0;
-    height: 25vh;
-    background-color: white;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 0;
+  padding-top: 0;
+  height: 25vh;
+  background-color: white;
+  text-align: center;
 `
 
 const FormRow = styled.div`
-    margin: 0 1rem;
+  margin: 0 1rem;
 `
-
 
 const Input = styled.input`
-    margin: 0 1rem;
-    height: 38px;
-    border-radius: 4px;
-    padding: 0 .5rem;
-    border: 2px solid ${accent};
+  margin: 0 1rem;
+  height: 38px;
+  border-radius: 4px;
+  padding: 0 0.5rem;
+  border: 2px solid ${accent};
 `
-
 
 //   .FooterSection h3 {
 //     padding: 10px;
@@ -99,4 +94,3 @@ const Input = styled.input`
 //   .FooterSection a:hover {
 //     color: grey;
 //   }
-  
--- src/components/ProfileInformation.js
+++ src/components/ProfileInformation.js
@@ -1,5 +1,4 @@
-import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from "react-icons/fa";
-
+import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from "react-icons/fa"
 
 //Import images for your portfolio here:
 //-- For best display purposes, each of these images should have similar porportions
@@ -10,115 +9,104 @@ import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from "react-icons/fa";
 // import taskyv from "../images/taskyv_site.png";
 // import bookThinks from "../images/bookThinks.png";
 
-import resume from "../assets/Resume.pdf";
+import resume from "../assets/Resume_N_Norris.pdf"
 
-let darkModeSwitch = true;
-let darkMode = false;
+let darkModeSwitch = true
+let darkMode = false
 let accent = "#003344"
 
 // Import image of your self here:
 //-- For best display purposes, your image should be sqaure (e.g. 300 X 300 px)
-const selfie = "trevor.png";
-const recrootBox = "recrootBox_site.png";
-const campRank =  "camprank_site.png";
-const blogApp = "blogapp-site.png";
-const taskyv = "taskyv_site.png";
-const bookThinks = "bookThinks.png";
-
-
+const selfie = "download.png"
+const recrootBox = "recrootBox_site.png"
+const campRank = "camprank_site.png"
+const blogApp = "blogapp-site.png"
+const taskyv = "taskyv_site.png"
+const bookThinks = "bookThinks.png"
 
 // Edit person info here
 // -- Bio is best kept to around 5 lines
-let firstName = "Trevor";
-let lastName = "Lane";
-let title = "Student / Software Developer";
-let bio = `Currently studying Computer Science at Brigham Young University 
-and working as a software engineering intern at a company called SimpleNexus 
-where I work with Vue.js & Rails. 
-Feel free to check out some of my past projects below, browse some code on my github profile, or just reach out to say hi!`;
+let firstName = "Nicholas"
+let lastName = "Norris"
+let title = "Student / Software Engineering Intern"
+let bio = `Junior studying Software Design & Development at the University of West Florida. Intern at Navy Federal Credit Union. Trying to be better than yesterday.`
 
 //Edit links to external accounts or items here (e.g. github, linkedIn, leetcode, resume, etc.)
 // -- For display purposes, it's best to use between 3 and 5 links, but if you add more or less,
 // -- the page will adjust accordingly.
 
-let userIcons = true;
+let userIcons = true
 
 let links = [
   {
     name: "Email",
-    link: "mailto:trevor@trevorlane.net",
-    icon: FaEnvelope
+    link: "mailto:nickjamesnorris@gmail.com",
+    icon: FaEnvelope,
   },
   {
     name: "GitHub",
-    link: "https://github.com/trevorhere",
-    icon: FaGithub
-
+    link: "https://github.com/nickjamesnorris",
+    icon: FaGithub,
   },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/hi-there-trevor-here/",
-    icon: FaLinkedin
-
+    link: "https://www.linkedin.com/in/nicholas-j-norris/",
+    icon: FaLinkedin,
   },
   {
     name: "Resume",
     link: resume,
-    icon: FaFileAlt
-
-  }
-];
+    icon: FaFileAlt,
+  },
+]
 
 //Edit portfolio items here by changing title, image title, and link
 // -- you can adjust the number of items as needed. For display purposes, it's best to use
 // -- multiples of 3.
 
 let portfolioTitle = "Recent Projects"
-let portfolioSectionBackgroundColor = "#003344"
+let portfolioSectionBackgroundColor = "#9370DB"
 let portfolioItems = [
   {
     imageUrl: taskyv,
     title: "Taskyv",
     description: "Sleek, sexy task manager",
-    link: "https://taskyv.herokuapp.com/"
+    link: "https://taskyv.herokuapp.com/",
   },
   {
     imageUrl: bookThinks,
     title: "BookThinks",
     description: "Book-centric note recorder",
-    link: "https://bookthinks.herokuapp.com/"
+    link: "https://bookthinks.herokuapp.com/",
   },
   {
     imageUrl: recrootBox,
     title: "Recroot Box",
     description: "Easy to user porfolio-site template",
-    link: "https://github.com/trevorhere/recrootBox/"
+    link: "https://github.com/trevorhere/recrootBox/",
   },
   {
     imageUrl: campRank,
     title: "CampRank",
     description: "Post, rank and comment about your favorite campsites",
-    link: "https://desolate-falls-89173.herokuapp.com/"
+    link: "https://desolate-falls-89173.herokuapp.com/",
   },
   {
     imageUrl: blogApp,
     title: "Blog App",
     description: "Easy access blog app.",
-    link: "https://boiling-harbor-27900.herokuapp.com"
-  }
-];
+    link: "https://boiling-harbor-27900.herokuapp.com",
+  },
+]
 
-let showEmailCapture = true;
+let showEmailCapture = true
 
 // If you do not want the footer to be displayed (it just has a link to the github repo
 // -- for this template ) then you can set showFooter to false and it will go away.
-let showFooter = true;
+let showFooter = false
 
-
-
-
-let blogName = "The Intern MR";
-let renderBlog = true;
+let blogName = "The Intern MR"
+let renderBlog = false
 let gistUrl = `https://api.github.com/gists/68cc754fb298f3121b5b2b4cfaa754d4`
 
 export {
@@ -140,5 +128,5 @@ export {
   blogName,
   renderBlog,
   gistUrl,
-  campRank
-};
+  campRank,
+}
