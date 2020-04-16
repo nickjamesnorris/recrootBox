45426824bd505cc837a1f5488722d1ad2e8418cb
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-04-16 17:17:27 -0500

    Webpack config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,34 @@
 exports.onCreatePage = async ({ page, actions }) => {
-    const { createPage } = actions
-  
-    // Only update the `/app` page.
-    if (page.path.match(/^\/posts/)) {
-      // page.matchPath is a special key that's used for matching pages
-      // with corresponding routes only on the client.
-      page.matchPath = "/posts/*"
-  
-      // Update the page.
-      createPage(page)
-    }
+  const { createPage } = actions
+
+  // Only update the `/app` page.
+  if (page.path.match(/^\/posts/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+    page.matchPath = "/posts/*"
+
+    // Update the page.
+    createPage(page)
   }
+}
+
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/node-apis/
+//  */
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /tiny-slider-react/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
